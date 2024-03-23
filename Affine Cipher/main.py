3d3bb196e9231a7f50863821766bd2da8828026b
sarimbinwaseem <sarimbinwaseem2000@gmail.com>
2024-03-23 18:27:24 +0500

    Affine Cipher initialized

---
+++ Affine Cipher/main.py
@@ -0,0 +1,7 @@
+"""Affine Cipher implementation"""
+
+class AffineCipher():
+	"""docstring for AffineCipher"""
+	def __init__(self, arg):
+		super(AffineCipher, self).__init__()
+		self.arg = arg
---
+++ Affine Cipher/utils.py
@@ -0,0 +1,11 @@
+"""Utilities for Affine Cipher"""
+
+class AffineMaths():
+	"""This class contains mathematics for Affine Cipher.
+
+	gcd: calculates GCD of number and len(table).
+	mul_inverse: calculates multiplicative inverse of a number.
+	"""
+	def __init__(self):
+		super().__init__()
+		# self.arg = arg
