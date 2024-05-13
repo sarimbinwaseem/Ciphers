4de252660c26c285b08f1f4e5447578c83e73072
sarimbinwaseem <42667306+sarimbinwaseem@users.noreply.github.com>
2024-05-13 14:29:02 +0500

    sub key 1 implemented, sub key 2 has an error

--- SDES/main.py
+++ SDES/main.py
@@ -1,5 +1,6 @@
 """SDES Cipher"""
-
+from pprint import pprint
+from copy import deepcopy
 from tables import Tables
 
 class SDES(Tables):
@@ -9,9 +10,39 @@ class SDES(Tables):
     """
 
     def __init__(self):
-        super.__init__(self)
+        super().__init__()
         self.sub_key_1 = None
         self.sub_key_2 = None
+    
+    def split_key(self, data) -> tuple[list]:
+
+        to_slices: int = len(data) // 2
+        first_half: list = data[:to_slices]
+        second_half: list = data[to_slices:]
+
+        return (first_half, second_half)
+
+    def _shift_1_bit(self, first_half, second_half) -> tuple[list]:
+        
+        first_half.append(first_half.pop(0))
+        second_half.append(second_half.pop(0))
+
+        return (first_half, second_half)
+
+    def _shift_2_bit(self, first_half, second_half) -> tuple[list]:
+        
+        for _ in range(2):
+            first_half.append(first_half.pop(0))
+            second_half.append(second_half.pop(0))
+
+        return (first_half, second_half)
+
+    def get_subkey(self, e, for_p8):
+
+        permuted_8 = []
+        permuted_8 = [e[p] for p in self.P8]
+
+        return permuted_8
 
     def sub_key_generation(self, key: str | int):
         """
@@ -19,3 +50,39 @@ class SDES(Tables):
         P8 -> Shift 2 bit -> P8
         """
 
+        if len(key) != 10:
+            raise ValueError("Key lenght is not 10.")
+
+        e = {}
+        for index, k in enumerate(key):
+            e[index + 1] = k
+
+        pprint(e)
+
+        permuted_10 = []
+        permuted_10 = [e[p] for p in self.P10]
+        pprint(permuted_10)
+
+        first_half, second_half = self.split_key(permuted_10)
+        first_half, second_half = self._shift_1_bit(first_half, second_half)
+
+        print(first_half)
+        print(second_half)
+
+        for_p8 = deepcopy(first_half)
+        for_p8.extend(second_half)
+
+        print(f"For P8: {for_p8}")
+
+        self.sub_key_1 = self.get_subkey(e, for_p8)
+
+        first_half, second_half = self._shift_2_bit(first_half, second_half)
+        for_p8 = deepcopy(first_half)
+        for_p8.extend(second_half)
+        self.sub_key_2 = self.get_subkey(e, for_p8)
+
+        print(f"Sub Key 1: {self.sub_key_1}")
+        print(f"Sub Key 2: {self.sub_key_2}")
+
+sdes = SDES()
+sdes.sub_key_generation("1010111010")
--- SDES/tables.py
+++ SDES/tables.py
@@ -2,6 +2,6 @@ from dataclasses import dataclass
 
 @dataclass
 class Tables:
-    IP: list = [2, 5, 1, 6, 8, 4, 6]
-    P10: list = [3, 5, 2, 7, 4, 10, 1, 9, 8, 6]
-    P8: list = [6, 3, 7, 4, 8, 5, 10, 9]
+    IP = [2, 5, 1, 6, 8, 4, 6]
+    P10 = [3, 5, 2, 7, 4, 10, 1, 9, 8, 6]
+    P8 = [6, 3, 7, 4, 8, 5, 10, 9]
